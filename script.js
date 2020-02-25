ca47d4f14a142a1b5a5376672af41f9a1972dc50
joel <joel.torres.dev@gmail.com>
2020-02-25 13:58:59 -0800

    refactor

--- script.js
+++ script.js
@@ -1,56 +1,68 @@
+// 1) grabs all the elements by id
 const form = document.getElementById('form');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
-// Show input error message
+// 3) start writing your functions that are being called from within your 
+// event listener.
+
+// Show input Error message
 function showError(input, message) {
-    const formControl = input.parentElement;
-    formControl.className = 'form-control error';
-    const small = formControl.querySelector('small');
+    const formControl = input.parentElement
+    formControl.className = "form-control error"
+    const small = formControl.querySelector("small");
     small.innerHTML = message
 }
 
-// show success outline 
+// Show input success outline
 function showSuccess(input) {
-    formControl = input.parentElement;
-    formControl.className = 'form-control success';
+    const formControl = input.parentElement;
+    formControl.className = "form-control success";
 }
 
-// Check email is valid
-function isValidEmail(email)  {
+function isValidEmail() {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
 
-// Event listeners
+
+// 2) creates the event listener and passes it two parameters
+// a- 'submit'
+// b- and another function to run other events/functions
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
-    if (username.value === '') {
+    if(username.value === '') {
         showError(username, 'username is required');
     } else {
         showSuccess(username);
     }
 
-    if (email.value === '') {
+    if(email.value === '') {
         showError(email, 'email is required');
-    } else if (!isValidEmail(email.value)) {
+    } else if (isValidEmail(email.value)) {
         showError(email, 'email is not valid');
     } else {
         showSuccess(email);
     }
 
-    if (password.value === '') {
+    if(password.value === '') {
         showError(password, 'password is required');
     } else {
         showSuccess(password);
     }
 
-    if (password2.value === '') {
+    if(password2.value === '') {
         showError(password2, 'password2 is required');
     } else {
         showSuccess(password2);
     }
+
+
+    
 });
+
+
+
